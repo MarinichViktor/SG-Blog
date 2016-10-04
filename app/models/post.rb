1a82684fa18975e82c93fc708b07580e08c2f57e
Vikke <crueltyfrizz@gmail.com>
2016-10-04 12:06:39 +0300

    edit validation values in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
  validates :title , length: { minimum: 5 , maximum: 30}
- validates :body , length: { minimum: 30}
- validates_format_of :title,  :with => /\A[a-zA-Z\s\.\!\?]+\Z/
+ validates :body , length: { minimum: 200}
 end
