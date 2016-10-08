37ebb89a6111a74233dd0818e7bb3018efa92218
Vikke <crueltyfrizz@gmail.com>
2016-10-08 20:10:29 +0300

    add image to post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,8 @@ module PostsHelper
 
   def body_article(body, length=100)
     return "#{body[0..length]}..." if /\s/=~body[length+1]
-    body_article(body,length+1)
+    return body_article(body,length+1) unless body[length+1].nil?
+    "#{body[0..100]}..."
   end
 
 end
