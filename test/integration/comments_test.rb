7bab119dbcf6551c2f6db08cb9b73cac95f055a0
Vikke <crueltyfrizz@gmail.com>
2016-10-11 00:07:15 +0300

    Refactor text in test comments

--- test/integration/comments_test.rb
+++ test/integration/comments_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 
 class CommentsTest < ActiveSupport::TestCase
   def setup
-    @post=Post.create(title: 'Anarchy', body: 'Anarchy'*30)
+    @post=Post.create(title: 'AnarchyInUk', body: 'Anarchy'*30)
     visit "/posts/#{@post.id}"
   end
 
   def test_add_comment_to_current_post_and_see_comment_at_current_post_page
      assert_difference  "Post.find(@post.id).comments.count", 1 do
-     page.fill_in "comment_body", :with => 'Anarchy'*3
+     page.fill_in "comment_body", :with => 'Anarchy'
      click_on("comment_submit")
    end
   end
@@ -18,11 +18,12 @@ class CommentsTest < ActiveSupport::TestCase
     page.fill_in "comment_body", :with => 'Ana'
     click_on("comment_submit")
   end
+  end
 
   def test_add_comment_and_see_this_comment_on_post_page
      page.fill_in "comment_body", :with => "Hello , this is new comment to yours post"
      click_on("comment_submit")
      page.has_content? ("Hello , this is new comment to yours post")
    end
-  end
+
 end
