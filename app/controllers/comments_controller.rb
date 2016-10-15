5f822910573aad7ecaeac081acfcc1f7d5d1ad47
MarinichViktor <crueltyfrizz@gmail.com>
2016-10-15 17:41:54 +0300

    Adding comments without page reload

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params.require(:comment).permit(:text))
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.js
+    end
   end
 end
