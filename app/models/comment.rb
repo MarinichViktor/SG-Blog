bde32f88f62e52caec413d94a32ac81b9809c33e
MarinichViktor <crueltyfrizz@gmail.com>
2016-10-15 13:28:16 +0300

    Add latest_four function to show only latest four comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates :text , length: { minimum: 5, maximum:30}
+  def self.latest_four
+    order(created_at: :desc).limit(4)
+  end
 end
