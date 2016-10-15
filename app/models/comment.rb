d51e4b2a193fb61fb52d4f4a2c20df1e23c61e63
MarinichViktor <crueltyfrizz@gmail.com>
2016-10-15 15:52:27 +0300

    Create button to Show/Hide all comments without page reload

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates :text , length: { minimum: 5, maximum:30}
-  def self.latest_four
-    order(created_at: :desc).limit(4)
+  def self.first_two
+    order(created_at: :asc).limit(2)
   end
 end
