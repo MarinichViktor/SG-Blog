b7a26b193f5b6aaaa6c5aa3db6f80bb9df9c4325
MarinichViktor <crueltyfrizz@gmail.com>
2016-10-20 13:49:18 +0300

    Fix url regexp

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates :text , length: { minimum: 2}
-  validates_format_of :text, :without => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\Z/ix
+  validates_format_of :text, :without => /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
   validates_format_of :text, :without => /First post!/i
   def self.first_two
     order(created_at: :asc).limit(2)
